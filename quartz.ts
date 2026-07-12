c37fb7c929a711dfd5ecfb61f8ec708de98dd58d
Curiousjoe13 <Curiousjoe13@users.noreply.github.com>
2026-07-12 20:39:26 +0300

    Sort explorer by numbered file slugs

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,20 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import * as ExternalPlugin from "./.quartz/plugins"
+import type { ExplorerOptions } from "./.quartz/plugins"
+
+const explorerCollator = new Intl.Collator("ru", { numeric: true, sensitivity: "base" })
+
+ExternalPlugin.Explorer({
+  sortFn: ((a, b) => {
+    if (a.isFolder !== b.isFolder) return a.isFolder ? -1 : 1
+
+    const aSlug = typeof a.data?.slug === "string" ? a.data.slug : ""
+    const bSlug = typeof b.data?.slug === "string" ? b.data.slug : ""
+    const aName = aSlug.split("/").filter(Boolean).pop() ?? a.displayName
+    const bName = bSlug.split("/").filter(Boolean).pop() ?? b.displayName
+    return explorerCollator.compare(aName, bName)
+  }) satisfies ExplorerOptions["sortFn"],
+})
 
 const config = await loadQuartzConfig()
 export default config
